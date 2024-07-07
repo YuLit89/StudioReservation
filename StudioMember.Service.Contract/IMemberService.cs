b693e452b4bbbf49e28b1844db43483ff0c06ca8
yw <quahyuwei@hotmail.com>
2024-07-08 02:14:49 +0800

    implement member serivce

--- StudioMember.Service.Contract/IMemberService.cs
+++ StudioMember.Service.Contract/IMemberService.cs
@@ -1,12 +1,26 @@
-﻿using System;
+﻿
+using StudioMember.Service.Contract.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace StudioMember.Service.Contract
 {
-    public class Class1
+    [ServiceContract]
+    public interface IMemberService : IDisposable
     {
+        [OperationContract]
+        Member FindByUserName(string Username);
+
+        [OperationContract]
+        Member Find(string Id);
+
+        List<Member> GetAll();
+
+        [OperationContract]
+        int Update(string Id,string Email, string PhoneNumber);
     }
 }
--- StudioMember.Service.Proxy/MemberServiceProxy.cs
+++ StudioMember.Service.Proxy/MemberServiceProxy.cs
@@ -1,12 +1,265 @@
-﻿using System;
+﻿using NLog;
+using StudioMember.Service.Contract;
+using StudioMember.Service.Contract.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace StudioMember.Service.Proxy
 {
-    public class Class1
+    public class MemberServiceProxy : IMemberService
     {
+        readonly EndpointAddress _endpoint;
+        readonly ChannelFactory<IMemberService> _channelFactory;
+
+        public MemberServiceProxy(int port)
+        {
+            var url = $"net.tcp://localhost:{port}/MemberService";
+
+            var netTcpBinding = new NetTcpBinding { Security = { Mode = SecurityMode.None } };
+
+            _endpoint = new EndpointAddress(url);
+            _channelFactory = new ChannelFactory<IMemberService>(netTcpBinding);
+
+        }
+
+        public void Dispose()
+        {
+          
+        }
+
+        public Member Find(string Id)
+        {
+            IMemberService s = null;
+            try
+            {
+                s = _channelFactory.CreateChannel(_endpoint);
+
+                if (s != null)
+                {
+                    return s.Find(Id);
+                }
+
+                LogManager.GetCurrentClassLogger().Error($"Proxy Error -1 , Request {Id}");
+                return null;
+            }
+            catch (FaultException ex)
+            {
+                LogManager.GetCurrentClassLogger().Error($"Proxy Error -2 , Request {Id}");
+                return null;
+            }
+            catch (CommunicationException ex)
+            {
+                LogManager.GetCurrentClassLogger().Error($"Proxy Error -3 , Request {Id}");
+                return null;
+            }
+            catch (TimeoutException ex)
+            {
+                LogManager.GetCurrentClassLogger().Error($"Proxy Error -4 , Request {Id}");
+                return null;
+            }
+            catch (Exception ex)
+            {
+                LogManager.GetCurrentClassLogger().Error($"Proxy Error -5 , Request {Id}");
+                return null;
+            }
+            finally
+            {
+                CloseOrAbortServiceChannel((ICommunicationObject)s);
+            }
+        }
+
+        public Member FindByUserName(string Username)
+        {
+            IMemberService s = null;
+            try
+            {
+                s = _channelFactory.CreateChannel(_endpoint);
+
+                if (s != null)
+                {
+                    return s.Find(Username);
+                }
+
+                LogManager.GetCurrentClassLogger().Error($"Proxy Error -1 , Request {Username}");
+                return null;
+            }
+            catch (FaultException ex)
+            {
+                LogManager.GetCurrentClassLogger().Error($"Proxy Error -2 , Request {Username}");
+                return null;
+            }
+            catch (CommunicationException ex)
+            {
+                LogManager.GetCurrentClassLogger().Error($"Proxy Error -3 , Request {Username}");
+                return null;
+            }
+            catch (TimeoutException ex)
+            {
+                LogManager.GetCurrentClassLogger().Error($"Proxy Error -4 , Request {Username}");
+                return null;
+            }
+            catch (Exception ex)
+            {
+                LogManager.GetCurrentClassLogger().Error($"Proxy Error -5 , Request {Username}");
+                return null;
+            }
+            finally
+            {
+                CloseOrAbortServiceChannel((ICommunicationObject)s);
+            }
+        }
+
+        public List<Member> GetAll()
+        {
+            IMemberService s = null;
+            try
+            {
+                s = _channelFactory.CreateChannel(_endpoint);
+
+                if (s != null)
+                {
+                    return s.GetAll();
+                }
+
+                LogManager.GetCurrentClassLogger().Error($"Proxy Error -1");
+                return null;
+            }
+            catch (FaultException ex)
+            {
+                LogManager.GetCurrentClassLogger().Error($"Proxy Error -2");
+                return null;
+            }
+            catch (CommunicationException ex)
+            {
+                LogManager.GetCurrentClassLogger().Error($"Proxy Error -3");
+                return null;
+            }
+            catch (TimeoutException ex)
+            {
+                LogManager.GetCurrentClassLogger().Error($"Proxy Error -4");
+                return null;
+            }
+            catch (Exception ex)
+            {
+                LogManager.GetCurrentClassLogger().Error($"Proxy Error -5");
+                return null;
+            }
+            finally
+            {
+                CloseOrAbortServiceChannel((ICommunicationObject)s);
+            }
+        }
+
+        public int Update(string Id, string Email, string PhoneNumber)
+        {
+            IMemberService s = null;
+            try
+            {
+                s = _channelFactory.CreateChannel(_endpoint);
+
+                if (s != null)
+                {
+                    return s.Update(Id,Email,PhoneNumber);
+                }
+
+                LogManager.GetCurrentClassLogger().Error($"Proxy Error -1 , Request {Id},{Email},{PhoneNumber}");
+                return -1;
+            }
+            catch (FaultException ex)
+            {
+                LogManager.GetCurrentClassLogger().Error($"Proxy Error -2 , Request {Id},{Email},{PhoneNumber}");
+                return -2;
+            }
+            catch (CommunicationException ex)
+            {
+                LogManager.GetCurrentClassLogger().Error($"Proxy Error -3 , Request {Id},{Email},{PhoneNumber}");
+                return -3;
+            }
+            catch (TimeoutException ex)
+            {
+                LogManager.GetCurrentClassLogger().Error($"Proxy Error -4 , Request {Id},{Email},{PhoneNumber}");
+                return -4;
+            }
+            catch (Exception ex)
+            {
+                LogManager.GetCurrentClassLogger().Error($"Proxy Error -5 , Request {Id},{Email},{PhoneNumber}");
+                return -5;
+            }
+            finally
+            {
+                CloseOrAbortServiceChannel((ICommunicationObject)s);
+            }
+        }
+
+        void CloseOrAbortServiceChannel(ICommunicationObject communicationObject)
+        {
+            var isClosed = false;
+
+            if (communicationObject == null || communicationObject.State == CommunicationState.Closed)
+            {
+                return;
+            }
+
+            try
+            {
+                if (communicationObject.State != CommunicationState.Faulted)
+                {
+                    communicationObject.Close();
+                    isClosed = true;
+                }
+            }
+            catch (CommunicationException)
+            {
+                // Catch this expected exception so it is not propagated further.
+                // Perhaps write this exception out to log file for gathering statistics...
+            }
+            catch (TimeoutException)
+            {
+                // Catch this expected exception so it is not propagated further.
+                // Perhaps write this exception out to log file for gathering statistics...
+            }
+            catch (Exception)
+            {
+                // An unexpected exception that we don't know how to handle.
+                // Perhaps write this exception out to log file for support purposes...
+                //throw;
+            }
+            finally
+            {
+                // If State was Faulted or any exception occurred while doing the Close(), then do an Abort()
+                if (!isClosed)
+                {
+                    AbortServiceChannel(communicationObject);
+                }
+            }
+        }
+
+        void AbortServiceChannel(ICommunicationObject communicationObject)
+        {
+            try
+            {
+                communicationObject.Abort();
+            }
+            catch (Exception)
+            {
+                // An unexpected exception that we don't know how to handle.
+                // If we are in this situation:
+                // - we should NOT retry the Abort() because it has already failed and there is nothing to suggest it could be successful next time
+                // - the abort may have partially succeeded
+                // - the actual service call may have been successful
+                //
+                // The only thing we can do is hope that the channel's resources have been released.
+                // Do not rethrow this exception because the actual service operation call might have succeeded
+                // and an exception closing the channel should not stop the client doing whatever it does next.
+                //
+                // Perhaps write this exception out to log file for gathering statistics and support purposes...
+            }
+        }
+
     }
 }
