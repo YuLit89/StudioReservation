4f838156444b8306e59ee426a86e1acd2a69b40b
yw <quahyuwei@hotmail.com>
2024-07-07 22:35:59 +0800

    create member micro service file and nlog

---
+++ StudioMember.Service.Host/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudioMember.Service.Host
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
