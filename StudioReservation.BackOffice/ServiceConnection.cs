05dd3718f7c6a4d814dbe1fd8e7c4d78ebf7c2f6
yw <quahyuwei@hotmail.com>
2024-07-08 13:00:42 +0800

    create admin project and call service

---
+++ StudioReservation.BackOffice/ServiceConnection.cs
@@ -0,0 +1,22 @@
+﻿using StudioMember.Service.Contract;
+using StudioMember.Service.Proxy;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace StudioReservation.BackOffice
+{
+   
+    public static class ServiceConnection
+    {
+        public static IMemberService _memberService;
+
+        static ServiceConnection()
+        {
+            _memberService = new MemberServiceProxy(int.Parse(ConfigurationManager.AppSettings["member-service-port"]));
+        }
+
+    }
+}
