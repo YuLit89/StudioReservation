42b6fa77eb89f897243f0d003378e4ed9fbc05fa
yw <quahyuwei@hotmail.com>
2024-07-26 02:53:35 +0800

    create room type class / datamodel / ado

---
+++ StudioRoomType.DataModel/Room.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudioRoomType.DataModel
+{
+    public class Room
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public string Size { get; set; }
+        public string Style { get; set; }
+        public decimal Rate { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string CreateBy { get; set; }
+        public string UpdateBy { get; set; }
+        public DateTime UpdatedDate { get; set; }
+
+    }
+}
