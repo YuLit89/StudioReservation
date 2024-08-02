1aa14c741c7f0a5f7570d0b47c95dd234c2c04df
yw <quahyuwei@hotmail.com>
2024-08-03 00:36:57 +0800

    1. submenu 2. member view

--- StudioReservation.BackOffice/Controllers/MemberController.cs
+++ StudioReservation.BackOffice/Controllers/MemberController.cs
@@ -1,4 +1,5 @@
-﻿using StudioMember.Service.Contract;
+﻿using StudioMember.DataModel;
+using StudioMember.Service.Contract;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,15 +23,36 @@ namespace StudioReservation.BackOffice.Controllers
         {
             var result = _memberService.GetAll();
 
+            if (result.Error != 0)
+            {
+                ViewBag.ErrorCode = result.Error.ToString();
+                return View("Error");
+            }
             return View(result);
+        }
+        [HttpGet]
+        public ActionResult CreateGet()
+        {
+            MemberViewModel model = new MemberViewModel();
+
+            return View(model);
+        }
+
+
+        [HttpGet]
+        public ActionResult EditGet(string memberId)
+        {
+            return View();
+
         }
 
         [HttpGet]
         public ActionResult Detail(string id)
         {
-            var result = _memberService.Find(id);
+            //var result = _memberService.Find(id);
 
-            return View(result);
+            //return View(result);
+            return View();
         }
 
     }
