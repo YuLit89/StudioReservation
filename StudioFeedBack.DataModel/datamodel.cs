c6d65eb6053efbe329ab34161d6c9e8106b053e3
YL <User@LAPTOP-53BACGDQ>
2024-08-05 02:49:07 +0800

    create feedback data model and controller + mockup response

---
+++ StudioFeedBack.DataModel/datamodel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudioFeedBack.DataModel
+{
+    public class FeedBack //admin use
+    { 
+        public long Id { get; set; }
+        public string TicketId { get; set; }
+        public string Title { get; set; }
+        public string UserEmail { get; set; }
+        public string UserPhoneNumber {  get; set; }
+        public string Message {  get; set; }
+        public DateTime SubmitTime { get; set; }
+        public int Type { get; set; } // 1 = User  , 2 = Admin
+        public string Preference { get; set; } // email / whatapp / wechat / line / call 
+        public string ReplyName { get; set; } // store admin name when reply feedback else will empty
+        public bool IsReplyed { get; set; }
+        public bool IsCompleted { get; set; }
+
+        public FeedBack()
+        {
+            //TicketId = Guid.NewGuid().ToString();
+        }
+    }
+
+    public class FeedBackViewModel // admin page use
+    {
+        public List<FeedBack> FeedBacks { get; set; }
+        public int Error { get; set; }
+    }
+
+    public class SubmitFeedBack // user submit page use
+    {
+        public string Title { get; set; }
+        public string UserEmail { get; set; }
+        public string UserPhoneNumber { get; set; }
+        public string Message { get; set; }
+        public string Preference { get; set; } // email / whatapp / wechat / line / call 
+    }
+
+    public class SubmitFeedBackResponse 
+    {
+        public string TicketId { get; set; }
+        public int Error { get; set; }
+    }
+}
---
+++ StudioReservation.BackOffice/Controllers/FeedBackController.cs
@@ -0,0 +1,135 @@
+﻿using StudioFeedBack.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StudioReservation.BackOffice.Controllers
+{
+    public class FeedBackController : Controller
+    {
+        // GET: FeedBack
+        public ActionResult Index()
+        {
+           
+            var result = new FeedBackViewModel // only return type = 1
+            {
+                Error = 0 ,
+                FeedBacks = new List<FeedBack>
+                {
+                    new FeedBack
+                    {
+                        Id = 1,
+                        TicketId = Guid.NewGuid().ToString(),
+                        Title = "Payment issue",
+                        UserEmail = "",
+                        UserPhoneNumber = "60-177272973",
+                        Message = "payment already complete but history not found",
+                        SubmitTime = DateTime.Now,
+                        Type = 1,
+                        Preference = "Call",
+                        IsCompleted = false
+                    },
+                       new FeedBack
+                    {
+                        Id = 2,
+                        TicketId = Guid.NewGuid().ToString(),
+                        Title = "account issue",
+                        UserEmail = "",
+                        UserPhoneNumber = "60-177272973",
+                        Message = "payment already complete but history not found",
+                        SubmitTime = DateTime.Now,
+                        Type = 1,
+                        Preference = "Call",
+                        IsCompleted = false,
+                    }
+                }
+
+            };
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult GetDetail(long Id)
+        {
+            var id = Guid.NewGuid().ToString();
+            var result = new FeedBackViewModel //
+            {
+                Error = 0,
+                FeedBacks = new List<FeedBack>
+                {
+                    new FeedBack
+                    {
+                        Id = 1,
+                        TicketId = id,
+                        Title = "Payment issue",
+                        UserEmail = "",
+                        UserPhoneNumber = "60-177272973",
+                        Message = "payment already complete but history not found",
+                        SubmitTime = DateTime.Now,
+                        Type = 1,
+                        Preference = "Call",
+                        IsCompleted = false
+                    },
+                       new FeedBack
+                    {
+                        Id = 2,
+                        TicketId = id,
+                        Title = "",
+                        UserEmail = "",
+                        UserPhoneNumber = "",
+                        Message = "issue have been fixed , please refresh again",
+                        SubmitTime = DateTime.Now,
+                        Type = 2,
+                        Preference = "",
+                        IsReplyed = true,
+                        IsCompleted = false,
+                    },
+                            new FeedBack
+                    {
+                        Id = 3,
+                        TicketId = id,
+                        Title = "",
+                        UserEmail = "",
+                        UserPhoneNumber = "",
+                        Message = "OK , thank for help",
+                        SubmitTime = DateTime.Now,
+                        Type = 1,
+                        Preference = "",
+                        IsReplyed = true,
+                        IsCompleted = false,
+                    },
+                    new FeedBack
+                    {
+                        Id = 4,
+                        TicketId = id,
+                        Title = "",
+                        UserEmail = "",
+                        UserPhoneNumber = "",
+                        Message = "Welcome , have a nice day ",
+                        SubmitTime = DateTime.Now,
+                        Type = 2,
+                        Preference = "",
+                        IsReplyed = true,
+                        IsCompleted = true,
+                    }
+                }
+            };
+
+                return View(result);
+            }
+
+        [HttpPost]
+        public ActionResult Reply()
+        {
+
+
+           return View();
+        }
+
+
+    }
+}
